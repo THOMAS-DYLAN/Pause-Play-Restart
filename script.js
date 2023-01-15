462c577b20381f563561730785a5538651b26c8b
THOMAS-DYLAN <70673340+THOMAS-DYLAN@users.noreply.github.com>
2023-01-14 22:56:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
-console.log($(".word").text())
+//for the most part... things that were commented out are a part of an interval pause and play system.
+
+
+// console.log($(".word").text())
+var mute = false;
 var interval = false;
 var x;
 var audioElement = document.createElement('audio');
 audioElement.setAttribute('src', 'd69b841f-0202-4edd-a041-ce4667be7dd9.m4a');
+duration = audioElement.duration;
+// $(".word").css({ 'color': 'rgb(250, 250, 250, .8)', 'text-shadow': '5px 5px black' }).show().delay(2500)
+
 
 $(".refresh").click(() => {
    interval = false;
-   $(".word").text("Hi").css('color', 'white');
-   clearInterval
-   $(".button").css({ 'background-color': 'white' }).text("Play Me!");
+   $(".word").text("Hi");
+   clearInterval(x)
+   $(".button").text("Play Me!");
    audioElement.currentTime = 0;
    audioElement.pause()
 })
 
-$(".button").click(() => {
+$(".play").click(() => {
    if (interval == false) {
       interval = true
       audioElement.play();
-   } else {
+      // x = setInterval(() => {
+      //    switch ($(".word").text()) {
+      //       case "Hi":
+      //          $(".word").text("How");
+      //          break;
+      //       case "How":
+      //          $(".word").text("You");
+      //          break;
+      //       case "You":
+      //          $(".word").text("Doin'?");
+      //          break;
+      //       case "Doin'?":
+      //          $(".word").text("Hi");
+      //    }
+      // }, 800);
+   }
+})
+
+$(".pause").click(() => {
+   if (interval == true) {
       interval = false;
       audioElement.pause();
-      clearInterval(x)
+      // clearInterval(x)
    }
-   if (interval == false) {
-      $(".button").css({ 'background-color': 'red' }).text("");
-      clearInterval(x)
-   } else if (interval == true) {
-      $(".button").css({ 'background-color': 'green' }).text("");
-      x = setInterval(() => {
-         switch ($(".word").text()) {
-            case "Hi":
-               $(".word").text("How").css({ 'color': 'purple', 'text-shadow': '13px 0 white' });
-               break;
-            case "How":
-               $(".word").text("You").css({ 'color': 'orange' });
-               break;
-            case "You":
-               $(".word").text("Doin'?").css('color', 'purple');
-               break;
-            case "Doin'?":
-               $(".word").text("Hi").css({ 'color': 'white', "text-shadow": "none" });
-         }
-      }, 800);
+})
+
+$(".volume").click(()=>{
+   if(mute == false){
+      mute = true;
+      $(".volume").text("volume_off")
+      audioElement.volume = 0
+   }else{
+      mute = false;
+      $(".volume").text("volume_up")
+      audioElement.volume = 1
    }
-   console.log(interval);
 })
